6a0ac61ef56efdfd7502c89556c2810f67871e55
hant0505 <23020532@vnu.edu.vn>
2025-11-14 23:30:27 +0700

    #5 chạy local
    đổi về localhost 8080 chạy thử vs npm và mvn test mongodb hđong ko

--- job-quest-java/job-quest-frontend/src/api/axiosConfig.js
+++ job-quest-java/job-quest-frontend/src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:8080", // tuỳ bạn
+  baseURL: "http://localhost:8080", // tuỳ bạn
 });
 
 // Gắn token Bearer từ localStorage
@@ -14,4 +14,20 @@ api.interceptors.request.use((config) => {
 });
 
 export default api;
+// import axios from "axios";
+
+// const api = axios.create({
+//   baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:8080", // tuỳ bạn
+// });
+
+// // Gắn token Bearer từ localStorage
+// api.interceptors.request.use((config) => {
+//   const token = localStorage.getItem("token");
+//   if (token) {
+//     config.headers.Authorization = `Bearer ${token}`;
+//   }
+//   return config;
+// });
+
+// export default api;
 
