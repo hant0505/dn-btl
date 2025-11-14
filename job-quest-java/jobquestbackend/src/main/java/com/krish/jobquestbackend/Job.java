c070d3daee7c61c4a12c53f1f74ce1ccdd24e76b
hant0505 <23020532@vnu.edu.vn>
2025-11-14 21:22:14 +0700

    #4 test argocd

--- job-quest-java/jobquestbackend/src/main/java/com/krish/jobquestbackend/Job.java
+++ job-quest-java/jobquestbackend/src/main/java/com/krish/jobquestbackend/Job.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-// test
+// test argocd
 import java.util.List;
 
 @Document(collection = "jobs")
